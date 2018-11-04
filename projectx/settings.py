3db79a75ba0c16a47286cc61eb7cffd3dd50a60b
aswinmohan-123 <aswinmohan5555@gmail.com>
2018-11-04 11:14:52 +0530

    Added requirements and linked postgresql The site is up and running

--- projectx/settings.py
+++ projectx/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'projectx.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'testdb',
+        'USER': 'aswinmohan',
+        'PASSWORD': 'qburst',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
